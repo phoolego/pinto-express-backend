7abb21177a8637b5bb7790abda114532a72f7ca2
Phuritat Chatyalak <chphuri@gmail.com>
2021-10-17 18:24:33 +0700

    test: storage

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({
   extended: true,
 }));
+app.use(express.static('asset'));
 app.use(express.static(__dirname + '/public'));
 require('./routes/routes')(app);
 
