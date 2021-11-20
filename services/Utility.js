b5936ea8411e050af079ad36cd6e8edff7b91290
phoolego <chphuri@gmail.com>
2021-11-20 13:47:33 +0700

    fix: time format

--- services/Utility.js
+++ services/Utility.js
@@ -1,8 +1,8 @@
 module.exports = {
     getCurrentTime(){
-        // var tzoffset = (new Date()).getTimezoneOffset() * 60000; //offset in milliseconds
-        var tzoffset = -420 * 60000; // fix offset in milliseconds for Thailand
-        var localISOTime = (new Date(Date.now() - tzoffset)).toISOString();
+        var tzoffset = (new Date()).getTimezoneOffset() * 60000; //offset in milliseconds
+        // var tzoffset = -420 * 60000; // fixed offset in milliseconds for Thailand
+        var localISOTime = (new Date(Date.now() - tzoffset)).toISOString().slice(0, 19).replace('T', ' ');
         return localISOTime;
     },
 }
