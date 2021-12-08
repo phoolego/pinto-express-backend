3a91c7b4d2efb54901c9c0a78b61f664ec3cb04b
phoolego <chphuri@gmail.com>
2021-12-08 17:54:41 +0700

    fix : get order/pre-order

--- routes/routes/customer.js
+++ routes/routes/customer.js
@@ -9,7 +9,9 @@ module.exports = (app) => {
   app.get('/customer/get-sell-product/detail',auth.authorization,CustomerController.getSellProductDetail);
   app.get('/customer/get-pre-order-product/detail',auth.authorization,CustomerController.getPreOrderProductDetail);
 
+  app.get('/customer/pre-order',auth.authorization,CustomerController.getUserPreOrder);
   app.post('/customer/pre-order/create',auth.authorization,CustomerController.insertPreOrder);
 
+  app.get('/customer/order',auth.authorization,CustomerController.getUserOrder);
   app.post('/customer/order/create',auth.authorization,CustomerController.insertOrder);
 };
