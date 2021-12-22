a32b71cc275c02b45f6e1aab133a5ee8665932e2
phoolego <chphuri@gmail.com>
2021-12-22 21:16:45 +0700

    fix: dateFormat

--- services/Utility.js
+++ services/Utility.js
@@ -10,11 +10,29 @@ module.exports = {
         var localISOTime = (new Date(date - tzoffset)).toISOString().slice(0, 19).replace('T', ' ');
         return localISOTime;
     },
-    findWeekinMonth(date){
+    findWeekInMonth(date){
         week1 = new Date(date.getFullYear(),date.getMonth(),'1');
         week2 = new Date(date.getFullYear(),date.getMonth(),'8');
         week3 = new Date(date.getFullYear(),date.getMonth(),'15');
         week4 = new Date(date.getFullYear(),date.getMonth(),'22');
+        if(date >= week4){
+            return week4;
+        }
+        else if(date >= week3){
+            return week3;
+        }
+        else if(date >= week2){
+            return week2;
+        }
+        else{
+            return week1
+        }
+    },
+    findWeekendInMonth(date){
+        week1 = new Date(date.getFullYear(),date.getMonth(),'7');
+        week2 = new Date(date.getFullYear(),date.getMonth(),'14');
+        week3 = new Date(date.getFullYear(),date.getMonth(),'21');
+        week4 = new Date(date.getFullYear(),date.getMonth()+1,'0');
         if(date >= week4){
             return week4;
         }
